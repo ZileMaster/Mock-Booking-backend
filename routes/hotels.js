25a7f9ff96ef6118635baba08794b6a22030ced5
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-02 13:54:56 +0100

    finishing the CRUD functionalities for the hotel

--- routes/hotels.js
+++ routes/hotels.js
@@ -15,15 +15,62 @@ router.post("/", async (req, res) => {
     }catch(err){
         res.status(500).json(err);
     }
-})
+});
 
 //update
 
+router.put("/:id", async (req, res) => {
+
+    try{ 
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id, 
+            { $set: req.body }, 
+            { new:true }
+            );
+        res.status(200).json(updatedHotel);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 //delete
+
+router.delete("/:id", async (req, res) => {
+
+    try{ 
+        await Hotel.findByIdAndDelete(
+            req.params.id, 
+            );
+        res.status(200).json("Hotel has been deleted");
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 //get
 
+router.get("/:id", async (req, res) => {
+
+    try{ 
+        const hotel = await Hotel.findById(
+            req.params.id, 
+            );
+        res.status(200).json(hotel);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 //getAll
 
+router.get("/", async (req, res) => {
+
+    try{ 
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 export default router

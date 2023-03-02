45baacc06b7938109c01b5d2c7d924f0d26d7aa5
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-02 18:40:56 +0100

    Finishing login with bcrypt and securely giving info without isAdmin and password to the web

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,3 +18,20 @@ export const register = async (req, res, next) => {
         next(err);
     }
 }
+
+export const login = async (req, res, next) => {
+    try{    
+        const user = await User.findOne({username: req.body.username})
+        
+        if(!user) return next(createError(404, "User not found!"));
+        
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+        if(!isPasswordCorrect) return next(createError(400, "Incorect password or username"));
+
+        const {password, isAdmin, ...otherDetails} = user._doc;
+
+        res.status(200).json({...otherDetails});
+    }catch(err){
+        next(err);
+    }
+}

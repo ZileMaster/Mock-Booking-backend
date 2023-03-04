f8fc674241e1888f4db957db4779aac201728019
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-04 22:48:27 +0100

    Adding Rooms in model, adding rooms routes, and adding functionalities in it's controller

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,17 +1,28 @@
 import express from "express"; 
+import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from "../controllers/room.controller.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 
 //create
 
+router.post("/:hotelid", verifyAdmin, createRoom);
+
 //update
+
+router.put("/:id", verifyAdmin, updateRoom);
 
 //delete
 
+router.delete("/:id", verifyAdmin, deleteRoom);
+
 //get
+
+router.get("/:id", getRoom);
 
 //getAll
 
+router.get("/", getAllRooms);
 
 export default router
